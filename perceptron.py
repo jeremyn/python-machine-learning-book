11b471fd647eb09672697e9eb69e5e59244bafe4
Jeremy Nation <jeremy@jeremynation.me>
2016-03-11 12:17:17 -0500

    Add perceptron.py

---
+++ perceptron.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+class Perceptron(object):
+
+    def __init__(self, eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+        for _ in range(self.n_iter):
+            errors = 0
+            for x_i, target in zip(X, y):
+                update = self.eta * (target - self.predict(x_i))
+                self.w_[1:] += update * x_i
+                self.w_[0] = update
+                errors += int(update != 0.0)
+            self.errors_.append(errors)
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
