06b87330aaf1468bb282c2b162c017bf7b982b0b
Jeremy Nation <jeremy@jeremynation.me>
2016-03-12 10:03:51 -0500

    Move code into plot_iris_with_classifier

--- chapter_3.py
+++ chapter_3.py
@@ -9,6 +9,47 @@ from sklearn import datasets
 from visualization import plot_decision_regions
 
 
+def plot_iris_with_classifier(clf, print_accuracy=False):
+    iris = datasets.load_iris()
+    X = iris.data[:, [2, 3]]
+    y = iris.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.3,
+        random_state=0,
+    )
+
+    sc = StandardScaler()
+    sc.fit(X_train)
+    X_train_std = sc.transform(X_train)
+    X_test_std = sc.transform(X_test)
+
+    clf.fit(X_train_std, y_train)
+
+    y_pred = clf.predict(X_test_std)
+    if print_accuracy:
+        print("Misclassified samples: %d" % (y_test != y_pred).sum())
+        print("Accuracy: %.2f" % accuracy_score(y_test, y_pred))
+
+    X_combined_std = np.vstack((X_train_std, X_test_std))
+    y_combined = np.hstack((y_train, y_test))
+
+    plot_decision_regions(
+        X=X_combined_std,
+        y=y_combined,
+        classifier=clf,
+        test_index=range(105, 150),
+    )
+
+    plt.xlabel('petal length [standardized]')
+    plt.ylabel('petal width [standardized]')
+    plt.legend(loc='upper left')
+
+    plt.show()
+
+
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-z))
 
@@ -31,41 +72,6 @@ def plot_sigmoid():
 
 
 if __name__ == '__main__':
-    iris = datasets.load_iris()
-    X = iris.data[:, [2, 3]]
-    y = iris.target
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X,
-        y,
-        test_size=0.3,
-        random_state=0,
-    )
-
-    sc = StandardScaler()
-    sc.fit(X_train)
-    X_train_std = sc.transform(X_train)
-    X_test_std = sc.transform(X_test)
-
-    ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
-    ppn.fit(X_train_std, y_train)
-
-    y_pred = ppn.predict(X_test_std)
-    print("Misclassified samples: %d" % (y_test != y_pred).sum())
-    print("Accuracy: %.2f" % accuracy_score(y_test, y_pred))
-
-    X_combined_std = np.vstack((X_train_std, X_test_std))
-    y_combined = np.hstack((y_train, y_test))
-
-    plot_decision_regions(
-        X=X_combined_std,
-        y=y_combined,
-        classifier=ppn,
-        test_index=range(105, 150),
-    )
-
-    plt.xlabel('petal length [standardized]')
-    plt.ylabel('petal width [standardized]')
-    plt.legend(loc='upper left')
-
-    plt.show()
+    clf = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+    plot_iris_with_classifier(clf, print_accuracy=True)
+    # plot_sigmoid()
