a093e5ba861ee0c15f37b7ef0f413955c099ba3c
Jeremy Nation <jeremy@jeremynation.me>
2016-03-12 12:24:44 -0500

    Add plot_xor

--- chapter_3.py
+++ chapter_3.py
@@ -108,10 +108,26 @@ def plot_sigmoid():
     plt.show()
 
 
+def plot_xor():
+    np.random.seed(0)
+    X_xor = np.random.randn(200, 2)
+    y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
+    y_xor = np.where(y_xor, 1, -1)
+
+    svm = SVC(kernel='rbf', random_state=0, gamma=0.1, C=10.0)
+    svm.fit(X_xor, y_xor)
+    plot_decision_regions(X_xor, y_xor, classifier=svm)
+
+    plt.legend(loc='upper left')
+    plt.show()
+
+
 if __name__ == '__main__':
     # clf = Perceptron(n_iter=40, eta0=0.1, random_state=0)
     # clf = LogisticRegression(C=1000.0, random_state=0)
-    clf = SVC(kernel='linear', C=1.0, random_state=0)
-    plot_iris_with_classifier(clf)
+    # clf = SVC(kernel='linear', C=1.0, random_state=0)
+    # plot_iris_with_classifier(clf)
+
     # plot_sigmoid()
     # plot_lr_regularization()
+    plot_xor()
