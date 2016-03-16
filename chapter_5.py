2aee366e8604c6418312b1a1ed4e8993bb5cc1f0
Jeremy Nation <jeremy@jeremynation.me>
2016-03-16 12:34:22 -0400

    Add plot_sklearn_pca_with_lr

--- chapter_5.py
+++ chapter_5.py
@@ -4,7 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.cross_validation import train_test_split
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
+
+from visualization import plot_decision_regions
 
 
 def plot_manual_pca_transformation(X, y):
@@ -68,6 +72,51 @@ def plot_manual_pca_transformation(X, y):
     print(X_pca[0])
 
 
+def plot_sklearn_pca_with_lr(X_train, X_test, y_train, y_test):
+    pca = PCA()
+    pca.fit(X_train)
+    print(pca.explained_variance_ratio_)
+
+    plt.bar(
+        range(1, 14),
+        pca.explained_variance_ratio_,
+        alpha=0.5,
+        align='center',
+    )
+    plt.step(
+        range(1, 14),
+        np.cumsum(pca.explained_variance_ratio_),
+        where='mid',
+    )
+    plt.ylabel('Explained variance ratio')
+    plt.xlabel('Principal components')
+    plt.show()
+
+    pca = PCA(n_components=2)
+    X_train_pca = pca.fit_transform(X_train)
+    X_test_pca = pca.transform(X_test)
+
+    plt.scatter(X_train_pca[:, 0], X_train_pca[:, 1])
+    plt.xlabel('PC 1')
+    plt.ylabel('PC 2')
+    plt.show()
+
+    lr = LogisticRegression()
+    lr = lr.fit(X_train_pca, y_train)
+
+    plot_decision_regions(X_train_pca, y_train, classifier=lr)
+    plt.xlabel('PC 1')
+    plt.ylabel('PC 2')
+    plt.legend(loc='lower left')
+    plt.show()
+
+    plot_decision_regions(X_test_pca, y_test, classifier=lr)
+    plt.xlabel('PC 1')
+    plt.ylabel('PC 2')
+    plt.legend(loc='lower left')
+    plt.show()
+
+
 def get_standardized_wine_data():
     df = pd.read_csv(os.path.join('datasets', 'wine.data'), header=None)
     df.columns = [
@@ -92,4 +141,5 @@ def get_standardized_wine_data():
 
 if __name__ == '__main__':
     X_train, X_test, y_train, y_test = get_standardized_wine_data()
-    plot_manual_pca_transformation(X_train, y_train)
+    # plot_manual_pca_transformation(X_train, y_train)
+    plot_sklearn_pca_with_lr(X_train, X_test, y_train, y_test)
