d5cf4688b55c4f0ec2b5d77dbd053862fe0d7223
Jeremy Nation <jeremy@jeremynation.me>
2016-03-17 13:34:04 -0400

    Add plot_learning_curve

--- chapter_6.py
+++ chapter_6.py
@@ -1,5 +1,6 @@
 import os
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.cross_validation import (
@@ -8,6 +9,7 @@ from sklearn.cross_validation import (
     train_test_split,
 )
 from sklearn.decomposition import PCA
+from sklearn.learning_curve import learning_curve
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import (
@@ -29,6 +31,67 @@ def get_wdbc_data():
         random_state=1,
     )
     return X_train, X_test, y_train, y_test
+
+
+def plot_learning_curve(X_train, y_train):
+    pipe_lr = Pipeline([
+        ('scl', StandardScaler()),
+        ('clf', LogisticRegression(penalty='l2', random_state=0)),
+    ])
+
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator=pipe_lr,
+        X=X_train,
+        y=y_train,
+        train_sizes=np.linspace(0.1, 1.0, 10),
+        cv=10,
+    )
+
+    train_mean = np.mean(train_scores, axis=1)
+    train_std = np.std(train_scores, axis=1)
+    test_mean = np.mean(test_scores, axis=1)
+    test_std = np.std(test_scores, axis=1)
+
+    plt.plot(
+        train_sizes,
+        train_mean,
+        color='blue',
+        marker='o',
+        markersize=5,
+        label='training accuracy',
+    )
+    plt.fill_between(
+        train_sizes,
+        train_mean + train_std,
+        train_mean - train_std,
+        alpha=0.15,
+        color='blue',
+    )
+
+    plt.plot(
+        train_sizes,
+        test_mean,
+        color='green',
+        linestyle='--',
+        marker='s',
+        markersize=5,
+        label='validation accuracy',
+    )
+    plt.fill_between(
+        train_sizes,
+        test_mean + test_std,
+        test_mean - test_std,
+        alpha=0.15,
+        color='green',
+    )
+
+    plt.grid()
+    plt.xlabel('Number of training samples')
+    plt.ylabel('Accuracy')
+    plt.legend(loc='lower right')
+    plt.ylim([0.8, 1.0])
+
+    plt.show()
 
 
 def use_kfold_cross_validation(X_train, X_test, y_train, y_test):
@@ -65,4 +128,5 @@ def use_kfold_cross_validation(X_train, X_test, y_train, y_test):
 
 if __name__ == '__main__':
     X_train, X_test, y_train, y_test = get_wdbc_data()
-    use_kfold_cross_validation(X_train, X_test, y_train, y_test)
+    # use_kfold_cross_validation(X_train, X_test, y_train, y_test)
+    plot_learning_curve(X_train, y_train)
