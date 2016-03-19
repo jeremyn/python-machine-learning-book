b712a0084b4945196ac88682db8d8963077377ca
Jeremy Nation <jeremy@jeremynation.me>
2016-03-19 14:13:28 -0400

    Add run_grid_search

--- chapter_8.py
+++ chapter_8.py
@@ -1,8 +1,94 @@
+import os
+import re
+
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 import numpy as np
+import pandas as pd
 from sklearn.feature_extraction.text import (
     CountVectorizer,
     TfidfTransformer,
+    TfidfVectorizer,
 )
+from sklearn.grid_search import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+
+
+def preprocessor(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+    text = (
+        re.sub('[\W]+', ' ', text.lower()) +
+        ' '.join(emoticons).replace('-', '')
+    )
+    return text
+
+
+def tokenizer(text):
+    return text.split()
+
+
+def tokenizer_porter(text):
+    return [PorterStemmer().stem(word) for word in text.split()]
+
+
+def run_grid_search():
+    df = pd.read_csv(os.path.join('datasets', 'movie_data.csv'))
+    X_train = df.loc[:25000, 'review'].values
+    y_train = df.loc[:25000, 'sentiment'].values
+    X_test = df.loc[25000:, 'review'].values
+    y_test = df.loc[25000:, 'sentiment'].values
+
+    stop = stopwords.words('english')
+
+    tfidf = TfidfVectorizer(
+        strip_accents=None,
+        lowercase=None,
+        preprocessor=None,
+    )
+
+    param_grid = [
+        {
+            'vect__ngram_range': [(1, 1)],
+            'vect__stop_words': [stop, None],
+            'vect__tokenizer': [tokenizer, tokenizer_porter],
+            'clf__penalty': ['l1', 'l2'],
+            'clf__C': np.logspace(0, 2, num=3),
+        },
+        {
+            'vect__ngram_range': [(1, 1)],
+            'vect__stop_words': [stop, None],
+            'vect__tokenizer': [tokenizer, tokenizer_porter],
+            'vect__use_idf': [False],
+            'vect__norm': [None],
+            'clf__penalty': ['l1', 'l2'],
+            'clf__C': np.logspace(0, 2, num=3),
+        },
+    ]
+
+    lr_tfidf = Pipeline([
+        ('vect', tfidf),
+        ('clf', LogisticRegression(random_state=0)),
+    ])
+
+    gs_lr_tfidf = GridSearchCV(
+        lr_tfidf,
+        param_grid,
+        scoring='accuracy',
+        cv=5,
+        verbose=1,
+        n_jobs=-1,
+    )
+
+    gs_lr_tfidf.fit(X_train, y_train)
+
+    print(gs_lr_tfidf)
+    print("Best parameter set: %s" % gs_lr_tfidf.best_params_)
+    print("CV accuracy: %.3f" % gs_lr_tfidf.best_score_)
+
+    clf = gs_lr_tfidf.best_estimator_
+    print("Test accuracy: %.3f" % clf.score(X_test, y_test))
 
 
 def work_with_simple_bag_of_words():
@@ -36,4 +122,5 @@ def work_with_simple_bag_of_words():
 
 if __name__ == '__main__':
     np.random.seed(0)
-    work_with_simple_bag_of_words()
+    # work_with_simple_bag_of_words()
+    run_grid_search()
