b37a962f9f9ec4f64af52800772f440bae242752
Jeremy Nation <jeremy@jeremynation.me>
2016-03-20 13:46:17 -0400

    Add flask_app_2

---
+++ flask_app_2/app.py
@@ -0,0 +1,36 @@
+from flask import (
+    Flask,
+    render_template,
+    request,
+)
+from wtforms import (
+    Form,
+    TextAreaField,
+    validators,
+)
+
+app = Flask(__name__)
+
+
+class HelloForm(Form):
+    sayhello = TextAreaField('', [validators.DataRequired()])
+
+
+@app.route('/')
+def index():
+    form = HelloForm(request.form)
+    return render_template('first_app.html', form=form)
+
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    form = HelloForm(request.form)
+    return_val = render_template('first_app.html', form=form)
+    if form.validate():
+        name = request.form['sayhello']
+        return_val = render_template('hello.html', name=name)
+    return return_val
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
