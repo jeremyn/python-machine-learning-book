adce5c70ca7d11d472477d7f6c7851c6a8ad60da
Jeremy Nation <jeremy@jeremynation.me>
2016-03-21 18:37:58 -0400

    Add evaluate_sklearn_linear_regression

--- chapter_10.py
+++ chapter_10.py
@@ -4,9 +4,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import (
     LinearRegression,
     RANSACRegressor,
+)
+from sklearn.metrics import (
+    mean_squared_error,
+    r2_score,
 )
 from sklearn.preprocessing import StandardScaler
 
@@ -21,9 +26,9 @@ def get_housing_data():
         'CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX',
         'PTRATIO', 'B', 'LSTAT', 'MEDV',
     ]
-    X_rm = df[['RM']].values
+    X = df.iloc[:, :-1].values
     y = df['MEDV'].values
-    return df, X_rm, y
+    return df, X, y
 
 
 def visualize_housing_data(df):
@@ -172,11 +177,57 @@ def plot_ransac_linear_model(X, y):
     print("Intercept: %.3f" % ransac.estimator_.intercept_)
 
 
+def evaluate_sklearn_linear_regression(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.3,
+        random_state=0,
+    )
+    lr = LinearRegression()
+    lr.fit(X_train, y_train)
+    y_train_pred = lr.predict(X_train)
+    y_test_pred = lr.predict(X_test)
+
+    plt.scatter(
+        y_train_pred,
+        y_train_pred - y_train,
+        c='blue',
+        marker='o',
+        label='Training data',
+    )
+    plt.scatter(
+        y_test_pred,
+        y_test_pred - y_test,
+        c='lightgreen',
+        marker='s',
+        label='Test data',
+    )
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residuals')
+    plt.legend(loc='upper left')
+    plt.hlines(y=0, xmin=-10, xmax=50, lw=2, color='red')
+    plt.xlim([-10, 50])
+
+    plt.show()
+
+    print("MSE train: %.3f, test: %.3f" % (
+        mean_squared_error(y_train, y_train_pred),
+        mean_squared_error(y_test, y_test_pred),
+    ))
+    print("R^2 train: %.3f, test: %.3f" % (
+        r2_score(y_train, y_train_pred),
+        r2_score(y_test, y_test_pred),
+    ))
+
+
 if __name__ == '__main__':
-    df, X_rm, y = get_housing_data()
+    df, X, y = get_housing_data()
+    X_rm = df[['RM']].values
     # visualize_housing_data(df)
     sns.reset_orig()
     # plot_custom_linear_model(X_rm, y)
     # plot_sklearn_linear_model(X_rm, y)
     # create_linear_model_with_normal_equation(X_rm, y)
-    plot_ransac_linear_model(X_rm, y)
+    # plot_ransac_linear_model(X_rm, y)
+    evaluate_sklearn_linear_regression(X, y)
