fdb6b3d20c804f6babf15bfa153eab0aab8ddf49
Jeremy Nation <jeremy@jeremynation.me>
2016-03-21 15:28:11 -0400

    Add LinearRegressionGD and plotting functions

--- chapter_10.py
+++ chapter_10.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+from sklearn.preprocessing import StandardScaler
 
 
 def get_housing_data():
@@ -16,7 +17,9 @@ def get_housing_data():
         'CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX',
         'PTRATIO', 'B', 'LSTAT', 'MEDV',
     ]
-    return df
+    X_rm = df[['RM']].values
+    y = df['MEDV'].values
+    return df, X_rm, y
 
 
 def visualize_housing_data(df):
@@ -45,6 +48,68 @@ def visualize_housing_data(df):
     sns.reset_orig()
 
 
+class LinearRegressionGD(object):
+
+    def __init__(self, eta=0.001, n_iter=20):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        y = y.ravel()
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for _ in range(self.n_iter):
+            output = self.net_input(X)
+            errors = y - output
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors**2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        return self.net_input(X)
+
+
+def lin_regplot(X, y, model):
+    plt.scatter(X, y, c='lightblue')
+    plt.plot(X, model.predict(X), color='red', linewidth=2)
+
+
+def plot_custom_linear_model(X, y):
+    sc_X = StandardScaler()
+    sc_y = StandardScaler()
+    X_std = sc_X.fit_transform(X)
+    y_std = sc_y.fit_transform(y.reshape(-1, 1))
+
+    lr = LinearRegressionGD()
+    lr.fit(X_std, y_std)
+
+    plt.plot(range(1, lr.n_iter+1), lr.cost_)
+    plt.ylabel('SSE')
+    plt.xlabel('Epoch')
+
+    plt.show()
+
+    lin_regplot(X_std, y_std, lr)
+    plt.xlabel('Average number of rooms [RM] (standardized)')
+    plt.ylabel("Price in $1000's [MEDV] (standardized)")
+
+    plt.show()
+
+    print("Slope: %.3f" % lr.w_[1])
+    print("Intercept: %.3f" % lr.w_[0])
+
+    num_rooms_std = sc_X.transform([[5.0]])
+    price_std = lr.predict(num_rooms_std)
+    print("Price in $1000's: %.3f" % sc_y.inverse_transform(price_std))
+
+
 if __name__ == '__main__':
-    df = get_housing_data()
-    visualize_housing_data(df)
+    df, X_rm, y = get_housing_data()
+    # visualize_housing_data(df)
+    plot_custom_linear_model(X_rm, y)
