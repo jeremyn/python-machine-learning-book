056229ab7d226a1998491f353d54ad654044579f
Jeremy Nation <jeremy@jeremynation.me>
2016-03-22 18:21:53 -0400

    Plot silhouettes for k=2,3 and clusters for k=2

--- chapter_11.py
+++ chapter_11.py
@@ -1,6 +1,9 @@
+from matplotlib import cm
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
+from sklearn.metrics import silhouette_samples
 
 
 def get_blob_data():
@@ -13,6 +16,69 @@ def get_blob_data():
         random_state=0,
     )
     return X, y
+
+
+def plot_clusters(X, y, km):
+    point_info = (
+        ('lightgreen', 's', 'cluster 1'),
+        ('orange', 'o', 'cluster 2'),
+        ('lightblue', 'v', 'cluster 3'),
+    )
+    cluster_labels = np.unique(y)
+    for index, cluster_label in enumerate(cluster_labels):
+        c, marker, label = point_info[index]
+        plt.scatter(
+            X[y == cluster_label, 0],
+            X[y == cluster_label, 1],
+            s=50,
+            c=c,
+            marker=marker,
+            label=label,
+        )
+    plt.scatter(
+        km.cluster_centers_[:, 0],
+        km.cluster_centers_[:, 1],
+        s=250,
+        c='red',
+        marker='*',
+        label='centroids',
+    )
+
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+def plot_silhouettes(X, y):
+    cluster_labels = np.unique(y)
+    n_clusters = cluster_labels.shape[0]
+    silhouette_vals = silhouette_samples(X, y, metric='euclidean')
+    y_ax_lower = 0
+    y_ax_upper = 0
+    yticks = []
+    for i, c in enumerate(cluster_labels):
+        c_silhouette_vals = silhouette_vals[y == c]
+        c_silhouette_vals.sort()
+        y_ax_upper += len(c_silhouette_vals)
+        color = cm.jet(i / n_clusters)
+        plt.barh(
+            range(y_ax_lower, y_ax_upper),
+            c_silhouette_vals,
+            height=1.0,
+            edgecolor='none',
+            color=color,
+        )
+        yticks.append((y_ax_lower + y_ax_upper) / 2)
+        y_ax_lower += len(c_silhouette_vals)
+
+    silhouette_avg = np.mean(silhouette_vals)
+    plt.axvline(silhouette_avg, color='red', linestyle='--')
+
+    plt.yticks(yticks, cluster_labels + 1)
+    plt.ylabel('Cluster')
+    plt.xlabel('Silhouette coefficient')
+
+    plt.show()
 
 
 def plot_blob_data(X):
@@ -37,42 +103,7 @@ def plot_blob_data(X):
     )
     y_km = km.fit_predict(X)
 
-    plt.scatter(
-        X[y_km == 0, 0],
-        X[y_km == 0, 1],
-        s=50,
-        c='lightgreen',
-        marker='s',
-        label='cluster 1',
-    )
-    plt.scatter(
-        X[y_km == 1, 0],
-        X[y_km == 1, 1],
-        s=50,
-        c='orange',
-        marker='o',
-        label='cluster 2',
-    )
-    plt.scatter(
-        X[y_km == 2, 0],
-        X[y_km == 2, 1],
-        s=50,
-        c='lightblue',
-        marker='v',
-        label='cluster 3',
-    )
-    plt.scatter(
-        km.cluster_centers_[:, 0],
-        km.cluster_centers_[:, 1],
-        s=250,
-        c='red',
-        marker='*',
-        label='centroids',
-    )
-
-    plt.legend()
-    plt.grid()
-    plt.show()
+    plot_clusters(X, y_km, km)
 
     print("Distortion: %.2f" % km.inertia_)
 
@@ -92,6 +123,31 @@ def plot_blob_data(X):
     plt.ylabel('Distortion')
 
     plt.show()
+
+    km = KMeans(
+        n_clusters=3,
+        init='k-means++',
+        n_init=10,
+        max_iter=300,
+        tol=1e-04,
+        random_state=0,
+    )
+    y_km = km.fit_predict(X)
+
+    plot_silhouettes(X, y_km)
+
+    km = KMeans(
+        n_clusters=2,
+        init='k-means++',
+        n_init=10,
+        max_iter=300,
+        tol=1e-04,
+        random_state=0,
+    )
+    y_km = km.fit_predict(X)
+
+    plot_clusters(X, y_km, km)
+    plot_silhouettes(X, y_km)
 
 
 if __name__ == '__main__':
