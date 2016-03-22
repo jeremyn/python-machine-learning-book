ee84b9451d19c1f15c8a541a936f2147ab332233
Jeremy Nation <jeremy@jeremynation.me>
2016-03-22 17:27:00 -0400

    Add KMeans to plot_blob_data

--- chapter_11.py
+++ chapter_11.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
 
 
@@ -24,6 +25,53 @@ def plot_blob_data(X):
     )
     plt.grid()
 
+    plt.show()
+
+    km = KMeans(
+        n_clusters=3,
+        init='random',
+        n_init=10,
+        max_iter=300,
+        tol=1e-04,
+        random_state=0,
+    )
+    y_km = km.fit_predict(X)
+
+    plt.scatter(
+        X[y_km == 0, 0],
+        X[y_km == 0, 1],
+        s=50,
+        c='lightgreen',
+        marker='s',
+        label='cluster 1',
+    )
+    plt.scatter(
+        X[y_km == 1, 0],
+        X[y_km == 1, 1],
+        s=50,
+        c='orange',
+        marker='o',
+        label='cluster 2',
+    )
+    plt.scatter(
+        X[y_km == 2, 0],
+        X[y_km == 2, 1],
+        s=50,
+        c='lightblue',
+        marker='v',
+        label='cluster 3',
+    )
+    plt.scatter(
+        km.cluster_centers_[:, 0],
+        km.cluster_centers_[:, 1],
+        s=250,
+        c='red',
+        marker='*',
+        label='centroids',
+    )
+
+    plt.legend()
+    plt.grid()
     plt.show()
 
 
