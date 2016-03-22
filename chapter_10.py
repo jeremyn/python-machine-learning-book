41709095d13035bd0581c5529d2e4251f28515e0
Jeremy Nation <jeremy@jeremynation.me>
2016-03-22 10:28:16 -0400

    Add evaluate_housing_polynomial_models

--- chapter_10.py
+++ chapter_10.py
@@ -276,6 +276,87 @@ def evaluate_simple_polynomial_regression_model():
     ))
 
 
+def evaluate_housing_polynomial_models(X, y):
+    regr = LinearRegression()
+
+    quadratic = PolynomialFeatures(degree=2)
+    cubic = PolynomialFeatures(degree=3)
+    X_quad = quadratic.fit_transform(X)
+    X_cubic = cubic.fit_transform(X)
+
+    X_fit = np.arange(X.min(), X.max(), 1)[:, np.newaxis]
+
+    regr = regr.fit(X, y)
+    y_lin_fit = regr.predict(X_fit)
+    linear_r2 = r2_score(y, regr.predict(X))
+
+    regr = regr.fit(X_quad, y)
+    y_quad_fit = regr.predict(quadratic.fit_transform(X_fit))
+    quadratic_r2 = r2_score(y, regr.predict(X_quad))
+
+    regr = regr.fit(X_cubic, y)
+    y_cubic_fit = regr.predict(cubic.fit_transform(X_fit))
+    cubic_r2 = r2_score(y, regr.predict(X_cubic))
+
+    plt.scatter(X, y, label='training points', color='lightgray')
+
+    plt.plot(
+        X_fit,
+        y_lin_fit,
+        label="linear (d=1), $R^2=%.2f$" % linear_r2,
+        color='blue',
+        lw=2,
+        linestyle=':',
+    )
+    plt.plot(
+        X_fit,
+        y_quad_fit,
+        label="quadratic (d=2), $R^2=%.2f$" % quadratic_r2,
+        color='red',
+        lw=2,
+        linestyle='-',
+    )
+    plt.plot(
+        X_fit,
+        y_cubic_fit,
+        label="cubic (d=3), $R^2=%.2f$" % cubic_r2,
+        color='green',
+        lw=2,
+        linestyle='--',
+    )
+
+    plt.xlabel("% lower status of the population [LSTAT]")
+    plt.ylabel("Price in $1000's [MEDV]")
+    plt.legend(loc='upper right')
+
+    plt.show()
+
+    X_log = np.log(X)
+    y_sqrt = np.sqrt(y)
+
+    X_fit = np.arange(X_log.min()-1, X_log.max()+1, 1)[:, np.newaxis]
+
+    regr = regr.fit(X_log, y_sqrt)
+    y_lin_fit = regr.predict(X_fit)
+    linear_r2 = r2_score(y_sqrt, regr.predict(X_log))
+
+    plt.scatter(X_log, y_sqrt, label='training points', color='lightgray')
+
+    plt.plot(
+        X_fit,
+        y_lin_fit,
+        label="linear (d=1), $R^2=%.2f$" % linear_r2,
+        color='blue',
+        lw=2,
+    )
+
+    plt.xlabel('log(% lower status of the population [LSTAT])')
+    plt.ylabel("$\sqrt{Price \; in \; \$1000's [MEDV]}$")
+    plt.legend(loc='lower left')
+
+    plt.show()
+
+
 if __name__ == '__main__':
     df, X, y = get_housing_data()
     X_rm = df[['RM']].values
@@ -293,4 +374,6 @@ if __name__ == '__main__':
     )
     # evaluate_sklearn_linear_regression(X_train, X_test, y_train, y_test)
     # evaluate_sklearn_lasso_model(X_train, X_test, y_train, y_test)
-    evaluate_simple_polynomial_regression_model()
+    # evaluate_simple_polynomial_regression_model()
+    X_lstat = df[['LSTAT']].values
+    evaluate_housing_polynomial_models(X_lstat, y)
