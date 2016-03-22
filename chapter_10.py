3779d5ae461774a9153e90ad0e906718ec236803
Jeremy Nation <jeremy@jeremynation.me>
2016-03-22 10:27:08 -0400

    Add evaluate_simple_polynomial_regression_model

--- chapter_10.py
+++ chapter_10.py
@@ -14,7 +14,10 @@ from sklearn.metrics import (
     mean_squared_error,
     r2_score,
 )
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import (
+    PolynomialFeatures,
+    StandardScaler,
+)
 
 
 def get_housing_data():
@@ -233,6 +236,46 @@ def evaluate_sklearn_lasso_model(X_train, X_test, y_train, y_test):
     ))
 
 
+def evaluate_simple_polynomial_regression_model():
+    X = np.array(
+        [258, 270, 294, 320, 342, 368, 396, 446, 480, 586]
+    )[:, np.newaxis]
+    y = np.array(
+        [236.4, 234.4, 252.8, 298.6, 314.2, 342.2, 360.8, 368.0, 391.2, 390.8]
+    )
+
+    lr = LinearRegression()
+    pr = LinearRegression()
+    quadratic = PolynomialFeatures(degree=2)
+    X_quad = quadratic.fit_transform(X)
+
+    lr.fit(X, y)
+    X_fit = np.arange(250, 600, 10)[:, np.newaxis]
+    y_lin_fit = lr.predict(X_fit)
+
+    pr.fit(X_quad, y)
+    y_quad_fit = pr.predict(quadratic.fit_transform(X_fit))
+
+    plt.scatter(X, y, label='training points')
+    plt.plot(X_fit, y_lin_fit, label='linear fit', linestyle='--')
+    plt.plot(X_fit, y_quad_fit, label='quadratic fit')
+    plt.legend(loc='upper left')
+
+    plt.show()
+
+    y_lin_pred = lr.predict(X)
+    y_quad_pred = pr.predict(X_quad)
+
+    print("Training MSE linear: %.3f, quadratic: %.3f" % (
+        mean_squared_error(y, y_lin_pred),
+        mean_squared_error(y, y_quad_pred),
+    ))
+    print("Training R^2 linear: %.3f, quadratic %.3f" % (
+        r2_score(y, y_lin_pred),
+        r2_score(y, y_quad_pred),
+    ))
+
+
 if __name__ == '__main__':
     df, X, y = get_housing_data()
     X_rm = df[['RM']].values
@@ -249,4 +292,5 @@ if __name__ == '__main__':
         random_state=0,
     )
     # evaluate_sklearn_linear_regression(X_train, X_test, y_train, y_test)
-    evaluate_sklearn_lasso_model(X_train, X_test, y_train, y_test)
+    # evaluate_sklearn_lasso_model(X_train, X_test, y_train, y_test)
+    evaluate_simple_polynomial_regression_model()
