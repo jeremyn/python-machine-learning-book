9f4828bb10af503df1c471f2ff34331a9da2f7ad
Jeremy Nation <jeremy@jeremynation.me>
2016-03-23 11:22:36 -0400

    Add process_moon_data

--- chapter_11.py
+++ chapter_11.py
@@ -12,9 +12,13 @@ from scipy.spatial.distance import (
 )
 from sklearn.cluster import (
     AgglomerativeClustering,
+    DBSCAN,
     KMeans,
 )
-from sklearn.datasets import make_blobs
+from sklearn.datasets import (
+    make_blobs,
+    make_moons,
+)
 from sklearn.metrics import silhouette_samples
 
 
@@ -235,8 +239,86 @@ def process_random_data():
     print("Cluster labels: %s" % labels)
 
 
+def process_moon_data():
+    X, y = make_moons(n_samples=200, noise=0.05, random_state=0)
+    plt.scatter(X[:, 0], X[:, 1])
+    plt.show()
+
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 3))
+
+    km = KMeans(n_clusters=2, random_state=0)
+    y_km = km.fit_predict(X)
+    ax1.scatter(
+        X[y_km == 0, 0],
+        X[y_km == 0, 1],
+        c='lightblue',
+        marker='o',
+        s=40,
+        label='cluster 1',
+    )
+    ax1.scatter(
+        X[y_km == 1, 0],
+        X[y_km == 1, 1],
+        c='red',
+        marker='s',
+        s=40,
+        label='cluster 2',
+    )
+    ax1.set_title('K-means clustering')
+
+    ac = AgglomerativeClustering(
+        n_clusters=2,
+        affinity='euclidean',
+        linkage='complete',
+    )
+    y_ac = ac.fit_predict(X)
+    ax2.scatter(
+        X[y_ac == 0, 0],
+        X[y_ac == 0, 1],
+        c='lightblue',
+        marker='o',
+        s=40,
+        label='cluster 1',
+    )
+    ax2.scatter(
+        X[y_ac == 1, 0],
+        X[y_ac == 1, 1],
+        c='red',
+        marker='s',
+        s=40,
+        label='cluster 2',
+    )
+    ax2.set_title('Agglomerative clustering')
+
+    plt.legend()
+    plt.show()
+
+    db = DBSCAN(eps=0.2, min_samples=5, metric='euclidean')
+    y_db = db.fit_predict(X)
+    plt.scatter(
+        X[y_db == 0, 0],
+        X[y_db == 0, 1],
+        c='lightblue',
+        marker='o',
+        s=40,
+        label='cluster 1',
+    )
+    plt.scatter(
+        X[y_db == 1, 0],
+        X[y_db == 1, 1],
+        c='red',
+        marker='s',
+        s=40,
+        label='cluster 2',
+    )
+
+    plt.legend()
+    plt.show()
+
+
 if __name__ == '__main__':
     X_blob, y_blob = get_blob_data()
     # plot_blob_data(X_blob)
     np.random.seed(123)
-    process_random_data()
+    # process_random_data()
+    process_moon_data()
